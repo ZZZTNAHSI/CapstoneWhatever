65fafadcb94e023253fd7a8e15af7fb1ef2ced44
ZZZTnahsi <Ishantmahey1@gmail.com>
2024-07-20 18:26:38 -0500

    Add error handling and some comment giving a general overview of whats happening

--- index.js
+++ index.js
@@ -2,33 +2,45 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
+// useless consts that no body cares about
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// initialize server
 app.listen(port, () => {
     console.log(`Website is running on port ${port}.`);
 });
 
+// renders the server without waifus
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+// when the button is pressed, goes to post route /waifus to handle getting the waifus
 app.post("/waifus", async (req, res) => {
     let num = req.body.int;
-    if (num > 30) {
+    //  cant get more than 30 waifus at a time so redirects to home page if you do so 
+    if (num > 30 || num < 0) {
         res.redirect("/");
     }
     try {
-        const result = await axios.get("https://api.waifu.im/search?limit=" + num);
+        // This is where axios fetches the waifus
+        let result = 0
+        if (num === '1' || num === '0' || num == '') {
+            result = await axios.get("https://api.waifu.im/search");
+        } else {
+            result = await axios.get("https://api.waifu.im/search?limit=" + num);
+        }
         const info = result.data;
         res.render("index.ejs", {
             waifu: info.images
         });
     } catch (error) {
-        console.log(error.response.data);
-        res.status(500);
+        console.log(error.response);
+        res.render("index.ejs", {
+            error: "Something done fucked up"
+        });
       }
-     
 });
